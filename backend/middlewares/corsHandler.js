bb95bd1d53d43d11638a47a400c40d724d2aac16
Vartan Davtyan <vrtndvtn@yandex.ru>
2023-03-05 21:08:07 +0300

    cors test

--- backend/middlewares/corsHandler.js
+++ backend/middlewares/corsHandler.js
@@ -5,12 +5,10 @@ const corsHandler = (req, res, next) => {
   const { method } = req;
   const requestHeaders = req.headers['access-control-request-headers'];
 
-  /*   if (allowedCors.includes(origin)) {
-      res.header('Access-Control-Allow-Origin', origin);
-      res.header('Access-Control-Allow-Credentials', true);
-    } */
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Credentials', true);
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Credentials', true);
+  }
 
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
